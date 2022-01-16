d024bc641a1c3765ad09eb5cc63b9edfeee1509e
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2022-01-16 10:00:15 -0500

    Update fxmanifest.lua

--- RealismRPFramework/RRPF_core/fxmanifest.lua
+++ RealismRPFramework/RRPF_core/fxmanifest.lua
@@ -4,11 +4,13 @@ games {'gta5'}
 
 author 'Mega Utilities'
 description 'RealismRoleplayFramework'
-version 'DevPRV0.0.1A'
 
 -- What to run
 client_scripts {
-    'client.lua',
-    'client_two.lua'
+    '',
+    ''
 }
-server_script 'server.lua'
+server_scripts {
+    'RealismRPFramework/RRPF_core/versioncheck-dev.lua',
+    'RealismRPFramework/RRPF_core/versioncheck-stable.lua'
+}
