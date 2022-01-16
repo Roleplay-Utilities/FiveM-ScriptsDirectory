d519916364507c46e2d87391bfd42fda7bdeaf87
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2022-01-16 09:29:05 -0500

    Update versioncheck-stable.lua

--- RealismRPFramework/RRPF_core/versioncheck-stable.lua
+++ RealismRPFramework/RRPF_core/versioncheck-stable.lua
@@ -1,1 +1,20 @@
+Citizen.CreateThread( function()
+updatePath = "Roleplay-Utilities/ScriptsDirectory-FiveM/blob/main/RealismRPFramework/RRPF_core" -- your git user/repo path
+resourceName = "RealismRPFramework ("..GetCurrentResourceName()..")" -- the resource name
 
+function checkVersion(err,responseText, headers)
+	curVersion = LoadResourceFile(GetCurrentResourceName(), "version-stable") -- make sure the "version" file actually exists in your resource root!
+
+	if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+		print("𝕆𝕌𝕋𝔻𝔸𝕋𝔼𝔻")
+		print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
+		print("𝕆𝕌𝕋𝔻𝔸𝕋𝔼𝔻")
+	elseif tonumber(curVersion) > tonumber(responseText) then
+		print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+	else
+		print("\n"..resourceName.." is up to date, have fun!")
+	end
+end
+
+PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."version-stable", checkVersion, "GET")
+end)
